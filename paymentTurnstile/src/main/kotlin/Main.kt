7e692b59cf02156ec7d7e7a30bd44f185d41ee5a
RawStickk <rostislav.klimko@gmail.com>
2024-05-21 01:25:10 +0300

    Added Unlock event

--- paymentTurnstile/src/main/kotlin/Main.kt
+++ paymentTurnstile/src/main/kotlin/Main.kt
@@ -3,7 +3,6 @@ package paymentTurnstile
 import ru.nsk.kstatemachine.*
 import kotlinx.coroutines.*
 
-
 //class State(val price: Int = 0) : DefaultDataState<Int>(dataExtractor = defaultDataExtractor<Int>()) {
 //    var balance = 0
 //}
@@ -12,8 +11,9 @@ import kotlinx.coroutines.*
 //    object Unlocked : States()
 //}
 sealed class Events : Event {
-    data object Coin : Events()
+    class Coin(val coins: Int) : Events()
     data object Pass : Events()
+    data object Unlock: Events()
 }
 
 object Locked : DefaultState() {
@@ -27,19 +27,17 @@ object Locked : DefaultState() {
         balance += payment
     }
 
-    fun change() = if (balance - price > 0) balance - price else 0
+    fun change(): Int = if (balance - price > 0) balance - price else 0
     fun displayBalance() = println("Current balance: $balance coins")
     fun displayPrice() = println("Entry price: $price coins")
-    fun getBalance() = balance
-    fun getPrice() = price
+    fun getBalance(): Int = balance
+    fun getPrice(): Int = price
 }
 
 object Unlocked : DefaultState()
 
-//class Coin(override val data: Int) : DataEvent<Int>
-//class Pass(override val data: Int) : DataEvent<Int>
-
-class Customer(private var coins: Int = 0, private var passed: Boolean = false) {
+class Customer(private var coins: Int = 0) {
+    private var passed: Boolean = false
     fun getState() {
         println("Customer state:")
         println("Coins: $coins")
@@ -70,28 +68,24 @@ class Customer(private var coins: Int = 0, private var passed: Boolean = false) 
     fun coin(turnstile: StateMachine, payment: Int) = runBlocking {
         getState()
         println("COIN")
-        payCoins(payment)
         println("---------")
+        payCoins(payment)
+        turnstile.processEvent(Events.Coin(payment))
+        turnstile.processEvent(Events.Unlock)
         if (Unlocked.isActive)
             returnCoins(Locked.change())
-        turnstile.processEvent(Events.Coin, payment)
     }
 }
 
 fun main() = runBlocking {
-    val customer = Customer(10)
+    val customer = Customer(20)
     val turnstile = createStateMachine(this) {
         addState(Unlocked) {
             onEntry {
                 println("The turnstile is UNLOCKED.")
-                Locked.resetBalance()
-            }
-            transition<Events.Coin> {
-                onTriggered {
-                    println("The turnstile is already UNLOCKED. Please, take your coins back.")
-                }
             }
             transition<Events.Pass> {
+                Locked.resetBalance()
                 targetState = Locked
             }
         }
@@ -102,13 +96,14 @@ fun main() = runBlocking {
                 displayBalance()
             }
             transition<Events.Coin> {
-                //onTriggered is executed after direction = {}??????
                 onTriggered {
-                    Locked.processPayment(it.argument as Int)
+                    Locked.processPayment(it.event.coins)
+                    Locked.displayPrice()
+                    Locked.displayBalance()
                 }
-                Locked.displayPrice()
-                Locked.displayBalance()
-                guard = { Locked.getBalance() >= Locked.getPrice() }
+            }
+            transition<Events.Unlock> {
+                guard = {Locked.getBalance() >= Locked.getPrice()}
                 targetState = Unlocked
             }
             transition<Events.Pass> {
@@ -118,7 +113,7 @@ fun main() = runBlocking {
     }
     customer.pass(turnstile)
     customer.coin(turnstile, 4)
-    customer.coin(turnstile, 6)
+    customer.coin(turnstile, 8)
     customer.pass(turnstile)
     customer.getState()
 }
